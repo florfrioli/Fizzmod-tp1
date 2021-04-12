5e1474d79e1e0a19e45d09fe2beb8d16dc1ef473
florfrioli <florenciaf@live.com.ar>
2021-04-12 00:07:25 -0300

    punto 3 finalizado

--- tp.js
+++ tp.js
@@ -29,11 +29,42 @@ function urlRepo() {
 }
 
 /* 
-Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. La clase recibirá un texto que se guardará en una propiedad llamada texto. contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
+Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. 
+La clase recibirá un texto que se guardará en una propiedad llamada texto. 
+contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y 
+hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. 
+En ambos métodos, si el texto no es válido, se devolverá -1
 Crear un propiedad estática contadorInstancias que me indique cuantas instancias hay de esa clase.
 */
 const crearClase = () => {
-    return
+    class Clase {
+        constructor(texto) {
+            this.texto = texto;
+            Clase.contadorInstancias = (Clase.contadorInstancias || 0) + 1
+        }
+        contadorPalabras() {
+            if (typeof this.texto != "string") {
+                return -1;
+            } else {
+                const palabras = this.texto.trim().split(' ');
+                return (this.texto.trim() !== '') ? palabras.length : 0;
+            }
+        }
+        hayNumeros() {
+            let numeros = '0123456789';
+            if (typeof this.texto != "string") {
+                return -1;
+            } else {
+                for (let caracter of this.texto) {
+                    if (numeros.indexOf(caracter, 0) != -1) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return Clase;
 }
 
 module.exports = {
